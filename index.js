2c41c422ff6974901ae5e1b61e51fa17ef542338
Anissa Nishioka <ani.nishioka@berkeley.edu>
2024-01-12 18:26:13 -0800

    add text content

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ window.addEventListener('load', () => {
   highlightCurrentSection();
 });
 
-// detect which section user is currently viewing
+// get index for section user is currently viewing
 function detectSection() {
   const curScrollPos = this.scrollY;
   const viewHt = this.innerHeight;
@@ -20,12 +20,11 @@ function highlightCurrentSection() {
   const sectionIdx = detectSection();
 
   // remove active-link class from previous section
-  const prevActiveLink = document.querySelector('.nav-link.active-link');
-  console.log(prevActiveLink);
-  if (prevActiveLink) prevActiveLink.classList.remove('active-link');
+  const prevActiveLink = document.querySelector('.nav-link.active');
+  if (prevActiveLink) prevActiveLink.classList.remove('active');
 
-  //add active-link class to current section
-  navLinks[sectionIdx].classList.add('active-link');
+  //add active class to current section
+  navLinks[sectionIdx].classList.add('active');
 }
 
 window.addEventListener('scroll', highlightCurrentSection);
