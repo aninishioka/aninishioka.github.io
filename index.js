699ee300052e1c6e26a78cf77141a6e8d363bffc
Anissa Nishioka <ani.nishioka@berkeley.edu>
2024-01-13 17:29:36 -0800

    change highlighting current section link to use getBoundingClientRect for use w/ dynamic section sizes

--- index.js
+++ index.js
@@ -2,29 +2,37 @@ window.addEventListener('load', () => {
   highlightCurrentSection();
 });
 
-// get index for section user is currently viewing
-function detectSection() {
-  const curScrollPos = this.scrollY;
-  const viewHt = this.innerHeight;
-  const navHt = document.getElementById('site-nav').offsetHeight;
+// get nav link for section element user is currently viewing
+function getCurrentSectionLink() {
+  const navLinks = document.getElementsByClassName('nav-link');
 
   //get current section's index
-  const sectionIdx = Math.floor((curScrollPos - navHt) / viewHt) + 1;
-  return sectionIdx;
+  for (let link of navLinks) {
+    let sectionId = link.getAttribute('href').slice(1);
+    let section = document.getElementById(sectionId);
+    if (elemInView(section)) return link;
+  }
+}
 
+function elemInView(elem) {
+  const offset = document.getElementById('site-nav').offsetHeight;
+  let vh = window.innerHeight;
+  let rect = elem.getBoundingClientRect();
+  let top = rect.top - offset;
+  let bottom = rect.bottom - offset;
+  return ((top >= 0 && top <= .5 * vh) || (bottom <= vh && bottom > .5 * vh) || (top <= 0 && bottom >= vh));
 }
 
 // highlight link for section user is currently viewing
 function highlightCurrentSection() {
-  const navLinks = document.getElementsByClassName('nav-link');
-  const sectionIdx = detectSection();
+  let curLink = getCurrentSectionLink();
 
-  // remove active-link class from previous section
+  // remove active-link class from previous section link
   const prevActiveLink = document.querySelector('.nav-link.active');
   if (prevActiveLink) prevActiveLink.classList.remove('active');
 
-  //add active class to current section
-  navLinks[sectionIdx].classList.add('active');
+  //add active class to current section link
+  curLink.classList.add('active');
 }
 
 window.addEventListener('scroll', highlightCurrentSection);
